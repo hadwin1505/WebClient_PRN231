9ff173bd17f1163d27330be47015c0eed901ac16
Nineh09 <100771455+Nineh09@users.noreply.github.com>
2024-07-24 15:30:29 +0700

    Role

--- WebClient/obj/Debug/net7.0/WebClient.AssemblyInfo.cs
+++ WebClient/obj/Debug/net7.0/WebClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380453fb917cd00a6308e10cbe065a02ff2ab490")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
